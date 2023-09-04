16043bfe885659c29369f46f040bed6e36a869ca
A-kram <a-kram@yandex.ru>
2023-09-05 00:04:21 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './index.scss';
+import React from 'react';
 
 function App() {
+  const [count, SetCount] = React.useState(0);
+
+  const onClickPlus = () => {
+    SetCount(count + 1)
+  }
+
+  const onClickMinus = () => {
+    SetCount(count - 1)
+  }
+
+  
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">- Минус</button>
+        <button onClick={onClickPlus} className="plus">Плс +</button>
       </div>
     </div>
   );
